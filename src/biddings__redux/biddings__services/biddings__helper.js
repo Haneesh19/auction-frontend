a46498b2c00ef7271f7ac61e4e98e5ccf66b0a43
Haneesh19 <haneesh1901@gmail.com>
2024-05-09 23:07:18 +0530

    update

--- src/biddings__redux/biddings__services/biddings__helper.js
+++ src/biddings__redux/biddings__services/biddings__helper.js
@@ -1,2 +1,10 @@
 // export const BIDDING__MAIN__BACKENDURL="http://localhost:5000/api"
-export const BIDDING__MAIN__BACKENDURL= process.env.REACT_APP_API_PROXY
+// export const BIDDING__MAIN__BACKENDURL= process.env.REACT_APP_API_PROXY
+
+
+
+
+    export const BIDDING__MAIN__BACKENDURl =
+  import.meta.env.VITE_LOCAL_URL === "production"
+    ? "https://auction-backend-bchd.onrender.com/"
+    : `http://localhost:4000`;
