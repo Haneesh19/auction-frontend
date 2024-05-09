343762ab46dd55970ca1a2322bbb12d09b3dc667
Haneesh19 <haneesh1901@gmail.com>
2024-05-09 23:23:09 +0530

    update

--- src/biddings__redux/biddings__services/biddings__helper.js
+++ src/biddings__redux/biddings__services/biddings__helper.js
@@ -1,6 +1,5 @@
 // export const BIDDING__MAIN__BACKENDURL="http://localhost:5000/api"
-const dotenvConfig = require("dotenv").config(); // Load environment variables from .env file
-export const BIDDING__MAIN__BACKENDURL= process.env.REACT_APP_API_PROXY
+export const BIDDING__MAIN__BACKENDURL= process.env.REACT_APP_API_PROXY;
 
 
 
