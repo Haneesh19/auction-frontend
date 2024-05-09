a5560278a5453325de9b2e5fba6e38ca9244a79e
Haneesh19 <haneesh1901@gmail.com>
2024-05-09 22:53:46 +0530

    update

--- src/biddings__redux/biddings__services/biddings__helper.js
+++ src/biddings__redux/biddings__services/biddings__helper.js
@@ -1,1 +1,2 @@
-export const BIDDING__MAIN__BACKENDURL=process.env.REACT_APP_API_PROXY
+// export const BIDDING__MAIN__BACKENDURL="http://localhost:5000/api"
+export const BIDDING__MAIN__BACKENDURL= process.env.REACT_APP_API_PROXY
